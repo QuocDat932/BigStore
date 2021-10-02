1151770e673f52107492b63c3c9691d52045cc7f
huandiep <huan.diep@cyberlogitec.com>
2021-10-02 19:30:13 +0700

    Update delete function

---
+++ src/main/java/codejava/Dto/Message.java
@@ -0,0 +1,12 @@
+package codejava.Dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Message {
+    private String status;
+    private String success;
+    private String failure;
+}
