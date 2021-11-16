09fc4b92d4a9e09dbe5d1025b890f4310ce76e49
Bui Quoc Dat <datbui09032001@gmail.com>
2021-11-17 02:20:00 +0700

    develop2_FixUser v2

---
+++ src/main/java/codejava/API/APIADM/apiADMUserMgt.java
@@ -0,0 +1,32 @@
+package codejava.API.APIADM;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import codejava.Entity.Users;
+import codejava.Services.StatsServices;
+import codejava.Services.UserServices;
+
+@RestController
+@RequestMapping("/api/admin")
+public class apiADMUserMgt {
+	@Autowired
+	private StatsServices adminSrvs;
+	@Autowired
+	private UserServices userSrvs;
+	@GetMapping("/userMgt/ReloadDataUserChart")
+	public ResponseEntity<?> doGetReloadDataUser(){
+		String[][] dataUs = adminSrvs.getcountUs();
+		return ResponseEntity.ok(dataUs);
+	};
+	@GetMapping("/userMgt/ReloadDataUserTable")
+	public ResponseEntity<?> ReloadDataUserTable(){
+		List<Users> users = userSrvs.findAll();
+		return ResponseEntity.ok(users);
+	};
+}
