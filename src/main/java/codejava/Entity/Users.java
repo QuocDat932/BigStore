ee20a28259ffb2dac2bd879a3a82b093a2d55f3e
Duy182 <duyvaps12111@ftp.edu.vn>
2021-11-23 23:38:22 +0700

    Error

--- src/main/java/codejava/Entity/Users.java
+++ src/main/java/codejava/Entity/Users.java
@@ -66,6 +66,8 @@ public class Users implements Serializable {
 	@Column(name="phone")
 	private String phone;
 	
+	@Column(name="resetpassword_token")
+	private String resetpassword_token;
 	
 	@Column(name="address")
 	private String address;
