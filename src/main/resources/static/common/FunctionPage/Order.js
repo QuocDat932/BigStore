67f853725db0fd11c7207974837292abf8dc2d8d
Bui Quoc Dat <datbui09032001@gmail.com>
2021-12-15 01:40:13 +0700

    format_FormReject

--- src/main/resources/static/common/FunctionPage/Order.js
+++ src/main/resources/static/common/FunctionPage/Order.js
@@ -4,22 +4,22 @@
 // import :
 // <script type="text/javascript" th:src='@{/common/FunctionPage/Order.js}' ></script>
 /************************AJAX */
-TestOrd = () =>{
+TestOrd = () => {
     console.log("TestOrd");
 }
 // Get All Data Process
 let DataProcess = [];
-PgOrd_GetDataProcess = () =>{
+PgOrd_GetDataProcess = () => {
     let response = '/api/order/GetProcess'
     $.ajax({
         type: "GET",
-        url : response,
-        contentType:"application/json",
-        success: function (data){
+        url: response,
+        contentType: "application/json",
+        success: function (data) {
             DataProcess = data;
             PgOrd_sortByIdASC(DataProcess);
         },
-        error: function (e){
+        error: function (e) {
             Swal.fire({
                 icon: 'error',
                 text: "Can't load data !!!",
@@ -29,84 +29,98 @@ PgOrd_GetDataProcess = () =>{
 };
 
 // Sort By Id ASC
-PgOrd_sortByIdASC = (list) =>{
+PgOrd_sortByIdASC = (list) => {
     list.sort(function (a, b) {
-        return  a.id - b.id;
+        return a.id - b.id;
     });
 };
 // Sort By Id DESC
-PgOrd_sortByIdDESC = (list) =>{
+PgOrd_sortByIdDESC = (list) => {
     list.sort(function (a, b) {
         return - a.id + b.id;
     });
 };
 // From Process To Process
 PgOrd_frmProcessToProcess = (processId) => {
-let result = [];
-  if(processId === 1){
-    result.push(DataProcess[0], DataProcess[1]);
-  }
-  else if(processId === 2){
-    result.push(DataProcess[1], DataProcess[2]);
-  }
-  else if(processId === 3){
-    result.push(DataProcess[2], DataProcess[3]);
-  }
-  else if(processId === 4){
-    result.push(DataProcess[3]);
-  }
-  return result;
+    let result = [];
+    if (processId === 1) {
+        result.push(DataProcess[0], DataProcess[1]);
+    }
+    else if (processId === 2) {
+        result.push(DataProcess[1], DataProcess[2]);
+    }
+    else if (processId === 3) {
+        result.push(DataProcess[2], DataProcess[3]);
+    }
+    else if (processId === 4) {
+        result.push(DataProcess[3]);
+    }
+    return result;
 }
 
 /**********************************************************************ORDER - ORDER DETAIL****/
 let lstOrdDtl = [];
-PgOrd_getDataOrderDetailByOrderId = async (ordId) =>{
+PgOrd_getDataOrderDetailByOrderId = async (ordId) => {
     await PgOrd_getOrderDetailByOrderId(ordId);
     return lstOrdDtl;
 }
-PgOrd_getOrderDetailByOrderId = async (orderId) =>{
-    let url = '/api/order/getLstOrderDtl?ordId='+orderId;
+PgOrd_getOrderDetailByOrderId = async (orderId) => {
+    let url = '/api/order/getLstOrderDtl?ordId=' + orderId;
     await $.ajax({
-            type: "GET",
-            url : url,
-            contentType:"application/json",
-            success: function (data){
-                lstOrdDtl = data;
-            },
-            error: function (e){
-                Swal.fire({
-                    icon: 'error',
-                    text: "Can't load data !!!",
-                })
-            }
-        })
-};
-PgOrd_RejectOrder = async (ordId) =>{
-    let confirmReject = document.getElementById('confirmReject');
-    if(confirmReject.checked){
-        let reasonReject = $('#resionReject').val();
-        if(reasonReject.length <= 0 || reasonReject === '' || reasonReject === null){
-            alert(`Give Us Your Reasons , Please ! `);
-        }else{
-        let url = '/api/order/RejectOrder?ordId='+ordId+'&reasonReject='+reasonReject;
-        await $.ajax({
-                    type: "POST",
-                    url : url,
-                    contentType:"application/json",
-                    success: function (data){
-                        searchOrder();
-                    },
-                    error: function (e){
-                        Swal.fire({
-                            icon: 'error',
-                            text: "Can't load data !!!",
-                        })
-                    }
-                });
+        type: "GET",
+        url: url,
+        contentType: "application/json",
+        success: function (data) {
+            lstOrdDtl = data;
+        },
+        error: function (e) {
+            Swal.fire({
+                icon: 'error',
+                text: "Can't load data !!!",
+            })
         }
-    }else{
-        alert(`Check Reject PO, Please ! `);
+    })
+};
+PgOrd_RejectOrder = async (ordId) => {
+    let confirmReject = document.getElementById('confirmReject');
+    let reasonReject = $('#resionReject').val();
+    if (reasonReject.length <= 0 || reasonReject === '' || reasonReject === null) {
+        Swal.fire({
+            icon: 'warning',
+            text: `Please Give Us Reason Reject !`,
+        });
         return false;
+    }
+    else {
+        if (confirmReject.checked) {
+            let url = '/api/order/RejectOrder?ordId=' + ordId + '&reasonReject=' + reasonReject;
+            await $.ajax({
+                type: "POST",
+                url: url,
+                contentType: "application/json",
+                success: function (data) {
+                    searchOrder();
+                    OrderProcess(data)
+                    Swal.fire({
+                        icon: 'success',
+                        text: "Reject Successful",
+                    });
+                },
+                error: function (e) {
+                    Swal.fire({
+                        icon: 'error',
+                        text: "Can't load data !!!",
+                    });
+                }
+            });
+        }
+        else {
+            Swal.fire({
+                icon: 'warning',
+                text: `Please Check Confirm Reject !`,
+            })
+            return false;
+        }
     }
 };
 
